5d17d308bd94ce86edacb02ae37f7c9191f87f13
ericdo <ericdo.617@gmail.com>
2019-03-13 17:13:42 -0700

    Solved basics for parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -28,13 +28,16 @@ var parseJSON = function(json) {
   // If json is an object, e.g. '{"foo": ""}'
   if (json[0] === '{' && (json[json.length - 1] === '}')) {
     var obj = {};
+    var regex = /\"(.*?)\"/g
     var str = getBody(json);
+
     if (str.length > 0) {
+      str = replaceCommasInKeyVals(str);
+      console.log('string body: ' + str);
       var pairsArr = str.split(',');
       pairsArr.forEach(function(keyVal) {
         var propArr = keyVal.split(':');
         var key = propArr[0] ? clean(propArr[0]) : propArr[0];
-        console.log('key: ' + propArr[0]);
         var val = propArr[1] ? clean(propArr[1]) : propArr[1];
         obj[key] = val;
       })
@@ -44,12 +47,23 @@ var parseJSON = function(json) {
     return obj;
   }
 
+  function replaceCommasInKeyVals(str) {
+    return str.replace(regex, function(match) {
+             return match.replace(',', '##');
+           });
+  }
+
+  function replacePlaceholderWithComma(str) {
+    return str.replace('##', ',');
+  }
+
   function clean(string) {
     var str = string.trim().replace(/["]/g, '');
     if (!isNaN(str) && str.length > 0) {
       var number = +str;
       return number;
     }
+    str = replacePlaceholderWithComma(str);
     return checkLiteral(str);
   }
 
