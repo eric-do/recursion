f472b2a8cf84c01798bed952b84ce1362e4a6b0c
ericdo <ericdo.617@gmail.com>
2019-03-15 16:59:28 -0700

    A little refactor on functions

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -19,21 +19,17 @@ var parseJSON = function(json) {
     var str = getBody(json);
     if (str.length > 0) {
       arr = str.split(',').map(function(x) { return clean(x); });
-      console.log(arr);
     }
-    console.log('actual: ' + JSON.stringify(arr));
     return arr;
   }
 
   // If json is an object, e.g. '{"foo": ""}'
   if (json[0] === '{' && (json[json.length - 1] === '}')) {
     var obj = {};
-    var regex = /\"(.*?)\"/g
     var str = getBody(json);
 
     if (str.length > 0) {
-      str = replaceCommasInKeyVals(str);
-      console.log('string body: ' + str);
+      str = replaceCharactersInKeys(str, ',', '##');
       var pairsArr = str.split(',');
       pairsArr.forEach(function(keyVal) {
         var propArr = keyVal.split(':');
@@ -43,31 +39,35 @@ var parseJSON = function(json) {
       })
 
     }
-    console.log('actual: ' + JSON.stringify(obj));
     return obj;
   }
 
-  function replaceCommasInKeyVals(str) {
+  function replaceCharactersInKeys(str, original, placeholder) {
+    var regex = /\"(.*?)\"/g
     return str.replace(regex, function(match) {
-             return match.replace(',', '##');
+             return match.replace(original, placeholder);
            });
   }
 
-  function replacePlaceholderWithComma(str) {
-    return str.replace('##', ',');
-  }
-
   function clean(string) {
+    // Clean() will:
+      // Remove leading/trailing whitespaces
+      // Remove double quotes from string TODO this is not a good idea - CHANGE
+      // Return number if string is a number
+      // Return literal if it corresponds to null, true/false, undefined
+      // Else return string
     var str = string.trim().replace(/["]/g, '');
     if (!isNaN(str) && str.length > 0) {
       var number = +str;
       return number;
     }
-    str = replacePlaceholderWithComma(str);
+    str = str.replace('##', ',');
     return checkLiteral(str);
   }
 
   function checkLiteral(string) {
+    // This function checks to see if the string is an object literal
+    // and returns the literal otherwie returns the string
     if (string in literalMap) {
       return literalMap[string];
     }
